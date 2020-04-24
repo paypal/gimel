9610bd2e5d66f313bf8353037fb1cdf41d5e26c4
sathishsri88 <sathishsri88@gmail.com>
2020-04-24 23:22:01 +0530

    [236] Fixing Header comments

--- gimel-dataapi/gimel-common/src/main/scala/com/paypal/gimel/common/security/ColumnClassHandler.scala
+++ gimel-dataapi/gimel-common/src/main/scala/com/paypal/gimel/common/security/ColumnClassHandler.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 PayPal Inc.
+ * Copyright 2018 PayPal Inc.
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
